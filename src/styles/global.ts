0dea69c135f10ea7818c3e8dba7288b667a1e9ba
Anderson Almeida <andersonpa10pa@gmail.com>
2024-01-05 21:52:23 -0300

    Criação do tem default e dos estilos globais

---
+++ src/styles/global.ts
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle`
+   * {
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
+   }
+
+   :root {
+      font-size: 62.5%;
+   }
+
+   body {
+      background: ${({ theme }) => theme.background};
+      color: ${({ theme }) => theme['base-title']};
+   }
+
+   body, input-security, textarea, button{
+      /* font-family: 'Baloo 2', sans-serif; */
+      font-family: 'Roboto', sans-serif;  
+      font-size: 1.4rem;
+      line-height: 130%;
+      font-weight: 400;
+   }
+`
